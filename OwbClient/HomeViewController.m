c9c536b99965d5f0ccdb9b866b5bdb6df4a22e92
Jack-Tsue <jackzen1991@gmail.com>
2013-04-09 20:49:26 +0800

    new repo for client

---
+++ OwbClient/HomeViewController.m
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.m
+//  OwbClient
+//
+//  Created by  tsgsz on 4/7/13.
+//  Copyright (c) 2013 tsgsz. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "LoginViewController.h"
+
+@interface HomeViewController()
+
+@property (strong, nonatomic) LoginViewController* login_view_controller_;
+
+@end
+
+@implementation HomeViewController
+
+- (void)loadView
+{
+    self.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight)];
+    self.login_view_controller_ = [[LoginViewController alloc]initWithStyle:UITableViewStyleGrouped];
+    [self.view addSubview:self.login_view_controller_.view];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationLandscapeRight);
+}
+
+
+@end
---
+++ OwbClient/LoginViewController.m
@@ -0,0 +1,164 @@
+//
+//  LoginViewController.m
+//  OwbClient
+//
+//  Created by  tsgsz on 4/8/13.
+//  Copyright (c) 2013 tsgsz. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController () 
+@property  (nonatomic,strong) NSArray* labels;
+@end
+@implementation LoginViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+        self.labels = @[@"Username",@"Password"];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.frame = CGRectMake(260, 260, 100, 60);
+    [self.view setBackgroundColor:[UIColor clearColor]];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return [self.labels count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+        label.tag = 1;
+        label.highlightedTextColor = [UIColor whiteColor];
+        label.numberOfLines = 0;
+        label.opaque = NO;
+        label.backgroundColor = [UIColor clearColor];
+        [cell.contentView addSubview:label];
+        
+    }
+    
+    UILabel* label = (UILabel*)[cell viewWithTag:1];
+    NSString* title = [self.labels objectAtIndex:indexPath.row];
+    CGRect cellFrame = [cell frame];
+    cellFrame.origin = CGPointMake(10, 10);
+    label.text = title;
+    CGRect rect = CGRectInset(cellFrame,2,2);
+    label.frame = rect;
+    [label sizeToFit];
+    
+    UITextField *textField=[[UITextField alloc]initWithFrame:CGRectMake(120, 10, 260, 24)];
+    [textField setBorderStyle:UITextBorderStyleRoundedRect];
+    textField.returnKeyType = UIReturnKeyDone;
+    if (indexPath.row==1) {
+        textField.secureTextEntry = YES;
+    }
+    textField.delegate = self;
+    [cell.contentView addSubview:textField];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationLandscapeRight);
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+@end
