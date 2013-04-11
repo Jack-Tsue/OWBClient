a93f29e532df1ceb5732c3c4c4d99335b5a8d0f9
Jack-Tsue <jackzen1991@gmail.com>
2013-04-11 16:21:19 +0800

    correct code style

--- OwbClient/Controller/Home/HomeViewController.m
+++ OwbClient/Controller/Home/HomeViewController.m
@@ -13,7 +13,7 @@
 
 @property (strong, nonatomic) LoginViewController* login_view_controller_;
 
-@property (strong, nonatomic) UIView *testView; // test
+@property (strong, nonatomic) UIView *testView_; // test
 
 @end
 
@@ -26,15 +26,15 @@
     [self.view addSubview:self.login_view_controller_.view];
     
     // test
-    self.testView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Default.png"]];
-    self.testView.frame = CGRectMake(0, 726, 1024, 100);
+    self.testView_ = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Default.png"]];
+    self.testView_.frame = CGRectMake(0, 726, 1024, 100);
     UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc]
                                                 initWithTarget:self  
                                                 action:@selector(handlePan:)];
-    [self.testView setUserInteractionEnabled:YES];
-    [self.testView addGestureRecognizer:panGestureRecognizer];
+    [self.testView_ setUserInteractionEnabled:YES];
+    [self.testView_ addGestureRecognizer:panGestureRecognizer];
     [self.view setBackgroundColor:[UIColor whiteColor]];  
-    [self.view addSubview:self.testView];
+    [self.view addSubview:self.testView_];
     
 }
 
@@ -54,20 +54,20 @@
        ([recognizer state] == UIGestureRecognizerStateChanged) )
     {
         CGPoint movement = [recognizer translationInView:self.view];
-        CGRect old_rect = self.testView.frame;
+        CGRect old_rect = self.testView_.frame;
         
         old_rect.origin.y = old_rect.origin.y + movement.y;
         if(old_rect.origin.y < openFrame.origin.y)
         {
-            self.testView.frame = openFrame;
+            self.testView_.frame = openFrame;
         }
         else if(old_rect.origin.y > closeFrame.origin.y)
         {
-            self.testView.frame = closeFrame;
+            self.testView_.frame = closeFrame;
         }
         else
         {
-            self.testView.frame = old_rect;
+            self.testView_.frame = old_rect;
         }
         
         [recognizer setTranslation:CGPointZero inView:self.view];
@@ -75,11 +75,11 @@
     else if(recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled)
     {
         CGFloat halfPoint = (closeFrame.origin.y + openFrame.origin.y)/ 2;
-        if(self.testView.frame.origin.y > halfPoint)
+        if(self.testView_.frame.origin.y > halfPoint)
         {
             [UIView animateWithDuration:0.3f delay:0.0f options:UIViewAnimationOptionAllowUserInteraction|UIViewAnimationOptionBeginFromCurrentState|UIViewAnimationOptionCurveEaseOut animations:^{
                 
-                self.testView.frame = closeFrame;
+                self.testView_.frame = closeFrame;
             } completion:^(BOOL finished) {
                 
             }];
@@ -88,7 +88,7 @@
         {
             [UIView animateWithDuration:0.3f delay:0.0f options:UIViewAnimationOptionAllowUserInteraction|UIViewAnimationOptionBeginFromCurrentState|UIViewAnimationOptionCurveEaseOut animations:^{
                 
-                self.testView.frame = openFrame;
+                self.testView_.frame = openFrame;
             } completion:^(BOOL finished) {
                 
             }];
